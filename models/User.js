ec43f456158199786cdde0f89d4e7ad7af3ea909
Mary Paul <marymc135@hotmail.com>
2022-09-04 22:21:47 -0400

    hashing

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 // create our User model
@@ -45,6 +46,19 @@ User.init(
         }
       },
       {
+        
+        hooks: {
+         async beforeCreate(newUserData) {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+              return newUserData;
+            },
+
+            async beforeUpdate(updatedUserData) {
+              updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+              return updatedUserData; 
+            } 
+        },
+
         sequelize,
         timestamps: false,
         freezeTableName: true,
