8ffc76517a66a7dc5d36d1acb5406586b624c96c
Mary Paul <marymc135@hotmail.com>
2022-10-03 10:43:29 -0400

    login

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
@@ -7,6 +8,20 @@ const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+app.use(session(sess));
+
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
