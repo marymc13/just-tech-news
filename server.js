916a3b5876918003d53f98a161861d03c81fc800
Mary Paul <marymc135@hotmail.com>
2022-10-04 19:26:30 -0400

    helpers

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -21,6 +23,9 @@ const sess = {
   })
 };
 app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 
 app.engine('handlebars', hbs.engine);
